a99145949ec17b8860dd2ebf5bd8b4ab49a28dc3
Abdelrahman mohamed <abdhsrag280@gmail.com>
2025-06-07 14:57:58 +0300

    adjusted the style of impact section

--- src/components/common/impact.js
+++ src/components/common/impact.js
@@ -3,15 +3,15 @@ const ImpactSection = () => (
     <div className="container text-center">
       <h2>Our Impact</h2>
       <div className="row mt-4">
-        <div className="col-md-4">
+        <div className="col-md-3 impact-item">
           <h4>5,000+</h4>
           <p>Lives Transformed</p>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-3 m-auto impact-item">
           <h4>100+</h4>
           <p>Projects Completed</p>
         </div>
-        <div className="col-md-4">
+        <div className="col-md-3 impact-item">
           <h4>1,000+</h4>
           <p>Volunteers Involved</p>
         </div>
