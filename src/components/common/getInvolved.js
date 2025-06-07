82ca324beec3f9702e75a517954bdb01dcc6329b
Abdelrahman mohamed <abdhsrag280@gmail.com>
2025-06-07 14:59:40 +0300

    adjusted the style of get involved

--- src/components/common/getInvolved.js
+++ src/components/common/getInvolved.js
@@ -4,8 +4,8 @@ const GetInvolvedSection = () => (
     <h2>Get Involved</h2>
     <p>Join our community of changemakers and make a difference.</p>
     <div className="d-flex justify-content-center gap-3 mt-3">
-      <Link to="/volunteer" className="btn volunteer-btn">
-        Volunteer
+      <Link to="/projects" className="btn check-btn">
+        Check Projects
       </Link>
       <Link to="/donate" className="btn donate-btn">
         Donate
