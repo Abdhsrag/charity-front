6d3f017ec7db4d2560089d88fd7866e4cf04d45f
habiba <habibaahmed30>
2025-06-08 14:00:54 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+
 import ContactUs from './Components/ContactUs/ContactUs';
 import  './Components/ContactUs/ContactUs.css';
 import Footer from './Components/Footer/Footer';
 import './Components/Footer/Footer.css';
+import  NavBar  from './Components/NavBar/NavBar';
+import AboutUs from './Components/About/AboutUs';
+import NotFound from './Components/NotFound/NotFound';
 
 
 function App() {
   return (
     <div className="App">
-      <ContactUs />
-      <Footer/>
       <header className="App-header">
-        
+        <Router>
+                <NavBar/>
+          <Routes>
+            <Route path="/contactus" element={<ContactUs/>}/>
+            <Route path="/about" element={<AboutUs />}/>
+            <Route path="*" element={<NotFound/>}/>
+          </Routes>
+      <Footer/>
+      </Router>
       </header>
     </div>
   );
