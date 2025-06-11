af374aefb262f0b2a0a86f831ca3617c4ba3496c
Abdelrahman mohamed <abdhsrag280@gmail.com>
2025-06-11 12:02:54 +0300

    first merge

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
-
+import Navbar from "./components/navbar/navbar";
+import Home from "./components/home/home";
+import Footer from "./components/footer/footer";
 import ContactUs from './Components/ContactUs/ContactUs';
 import  './Components/ContactUs/ContactUs.css';
-import Footer from './Components/Footer/Footer';
 import './Components/Footer/Footer.css';
-import  NavBar  from './Components/NavBar/NavBar';
 import AboutUs from './Components/About/AboutUs';
 import NotFound from './Components/NotFound/NotFound';
 
